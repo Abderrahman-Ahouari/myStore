64ff6f320a14f7dba9fc86ef198279c5df6b144b
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-03-10 15:32:54 +0000

    creating all the migrations needed (migrations, modules)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,20 @@ class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
-}
+    protected $fillable = [
+        'section_id',
+        'name',
+        'description',
+        'price',
+        'stock',
+        'image',
+    ];
+
+    public function section(){
+        return $this->belongsto(section::class);
+    }
+
+    public function sales(){
+        return $this->hasMany(sale::class);
+    }
+}   
--- database/migrations/2025_03_10_152943_create_sales_table.php
+++ database/migrations/2025_03_10_152943_create_sales_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('section_id')->constrained()->onDelete('cascade');
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->integer('price');
-            $table->integer('stock')->default(0);
-            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('sales');
     }
 };
